af5196c7768cef285abc68f4922ab72f4c41f037
shun okada <falcon0296@gmail.com>
2023-02-09 20:58:20 +0900

    [feat] add padStart, padEnd

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+export const padStart = <T extends unknown>(
+  originalArray: T[],
+  length: number,
+  padValue: T
+): T[] => {
+  const padLength = length - originalArray.length;
+  if (padLength <= 0) {
+    return originalArray;
+  }
+  const padArray = [...Array(padLength)].map(() => padValue);
+  return [...padArray, ...originalArray];
+};
+
+export const padEnd = <T extends unknown>(
+  originalArray: T[],
+  length: number,
+  padValue: T
+): T[] => {
+  const padLength = length - originalArray.length;
+  if (padLength <= 0) {
+    return originalArray;
+  }
+  const padArray = [...Array(padLength)].map(() => padValue);
+  return [...originalArray, ...padArray];
+};
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,51 @@
-import { myPackage } from '../src';
+import { padStart, padEnd } from '../src';
 
 describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+  const originalArray = [
+    { id: 1, name: 'Bulbasaur' },
+    { id: 2, name: 'Ivysaur' },
+    { id: 3, name: 'Venusaur' },
+    { id: 4, name: 'Charmander' },
+    { id: 5, name: 'Charmeleon' },
+    { id: 6, name: 'Charizard' },
+  ];
 
-      const result = myPackage(message);
+  const pikachu = {
+    id: 25,
+    name: 'Pikachu',
+  };
 
-      expect(result).toMatch(message);
+  describe('padStart', () => {
+    it('should return original array if original array is longer than specified length.', () => {
+      const result = padStart(originalArray, originalArray.length - 1, pikachu);
+      expect(result).toEqual(originalArray);
+    });
+
+    it('should return original array if original array is the same length as the specified length.', () => {
+      const result = padStart(originalArray, originalArray.length, pikachu);
+      expect(result).toEqual(originalArray);
+    });
+
+    it('should return original array if specified length is longer than original array length.', () => {
+      const result = padStart(originalArray, originalArray.length + 3, pikachu);
+      expect(result).toEqual([pikachu, pikachu, pikachu, ...originalArray]);
+    });
+  });
+
+  describe('padEnd', () => {
+    it('should return original array if original array is longer than specified length.', () => {
+      const result = padEnd(originalArray, originalArray.length - 1, pikachu);
+      expect(result).toEqual(originalArray);
+    });
+
+    it('should return original array if original array is the same length as the specified length.', () => {
+      const result = padEnd(originalArray, originalArray.length, pikachu);
+      expect(result).toEqual(originalArray);
+    });
+
+    it('should return original array if specified length is longer than original array length.', () => {
+      const result = padEnd(originalArray, originalArray.length + 3, pikachu);
+      expect(result).toEqual([...originalArray, pikachu, pikachu, pikachu]);
     });
   });
 });
